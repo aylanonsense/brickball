a5fde76073a1a05e0974870246b12491a6fbbc61
Ayla Myers <bridgs.dev@gmail.com>
2019-06-26 16:31:45 -0400

    Starting with simulsim example game

---
+++ main.lua
@@ -0,0 +1,89 @@
+local simulsim = require 'https://raw.githubusercontent.com/bridgs/simulsim/3eabb5511ad651328db423d7143602f32d7afdd0/simulsim.lua'
+
+-- Define a new game
+local game = simulsim.defineGame()
+
+-- When the game is first loaded, set the background color
+function game.load(self)
+  self.data.backgroundColor = { 0.1, 0.1, 0.1 }
+end
+
+-- Update the game's state every frame by moving each entity
+function game.update(self, dt)
+  for _, entity in ipairs(self.entities) do
+    local inputs = self:getInputsForClient(entity.clientId) or {}
+    local moveX = (inputs.right and 1 or 0) - (inputs.left and 1 or 0)
+    local moveY = (inputs.down and 1 or 0) - (inputs.up and 1 or 0)
+    entity.x = math.min(math.max(0, entity.x + 200 * moveX * dt), 380)
+    entity.y = math.min(math.max(0, entity.y + 200 * moveY * dt), 380)
+  end
+end
+
+-- Handle events that the server and client fire, which may end up changing the game state
+function game.handleEvent(self, eventType, eventData)
+  -- Spawn a new player entity for a client
+  if eventType == 'spawn-player' then
+    self:spawnEntity({
+      clientId = eventData.clientId,
+      x = eventData.x,
+      y = eventData.y,
+      width = 20,
+      height = 20,
+      color = eventData.color
+    })
+  -- Despawn a player
+  elseif eventType == 'despawn-player' then
+    self:despawnEntity(self:getEntityWhere({ clientId = eventData.clientId }))
+  end
+end
+
+-- Create a client-server network for the game to run on
+local network, server, client = simulsim.createGameNetwork(game)
+
+-- When a client connects to the server, spawn a playable entity for them to control
+function server.clientconnected(self, client)
+  self:fireEvent('spawn-player', {
+    clientId = client.clientId,
+    x = 100 + 200 * math.random(),
+    y = 100 + 200 * math.random(),
+    color = { math.random(), 1, math.random() }
+  })
+end
+
+-- When a client disconnects from the server, despawn their player entity
+function server.clientdisconnected(self, client)
+  self:fireEvent('despawn-player', { clientId = client.clientId })
+end
+
+-- Every frame the client tells the server which buttons it's pressing
+function client.update(self, dt)
+  self:setInputs({
+    up = love.keyboard.isDown('w') or love.keyboard.isDown('up'),
+    left = love.keyboard.isDown('a') or love.keyboard.isDown('left'),
+    down = love.keyboard.isDown('s') or love.keyboard.isDown('down'),
+    right = love.keyboard.isDown('d') or love.keyboard.isDown('right')
+  })
+end
+
+-- Draw the game for each client
+function client.draw(self)
+  -- Clear the screen
+  love.graphics.setColor(self.game.data.backgroundColor)
+  love.graphics.rectangle('fill', 0, 0, 400, 400)
+  -- Draw each entity
+  for _, entity in ipairs(self.game.entities) do
+    love.graphics.setColor(entity.color)
+    love.graphics.rectangle('fill', entity.x, entity.y, entity.width, entity.height)
+  end
+  -- Draw the client's network status
+  love.graphics.setColor(1, 1, 1)
+  if self:isConnecting() then
+    love.graphics.print('Connecting...', 3, 3)
+  elseif not self:isConnected() then
+    love.graphics.print('Disconnected! :(', 3, 3)
+  elseif not self:isStable() then
+    love.graphics.print('Connected! Stabilizing...', 3, 3)
+  else
+    love.graphics.print('Connected! Frames of latency: ' .. self:getFramesOfLatency(), 3, 3)
+  end
+end
