049b9f1ec1f684c0df44e1c26c427f4942773d84
Ayla Myers <bridgs.dev@gmail.com>
2019-06-27 19:40:41 -0400

    Pick up and throw balls

--- main.lua
+++ main.lua
@@ -7,23 +7,28 @@ local game = simulsim.defineGame()
 
 function game.load(self)
   self:spawnEntity({
+    id = 'ball-1',
     type = 'ball',
-    x = GAME_WIDTH / 2 - 10,
-    y = GAME_HEIGHT / 2 - 10,
+    x = GAME_WIDTH / 2 - 4,
+    y = GAME_HEIGHT / 2 - 4,
     width = 8,
     height = 8,
-    vx = 100,
-    vy = -20
+    vx = 0,
+    vy = 0,
+    freezeFrames = 0,
+    isBeingHeld = false
   })
   for r = 1, 10 do
     for team = 1, 2 do
-      for c = 1, 6 do
+      for c = 1, 5 do
         self:spawnEntity({
+          id = 'brick-' .. c .. 'x' .. r .. '-' .. team,
           type = 'brick',
-          x = 6 * (c - 1) + (team == 1 and 12 or GAME_WIDTH - 6 * 6 - 12),
+          x = 6 * (c - 1) * (team == 1 and 1 or -1) + (team == 1 and 12 or GAME_WIDTH - 18),
           y = 12 + 12 * (r - 1),
           width = 6,
           height = 12,
+          color = c,
           team = team
         })
       end
@@ -38,9 +43,17 @@ function game.update(self, dt, isRenderable)
       local inputs = self:getInputsForClient(entity.clientId) or {}
       local moveX = (inputs.right and 1 or 0) - (inputs.left and 1 or 0)
       local moveY = (inputs.down and 1 or 0) - (inputs.up and 1 or 0)
-      local speedMult = moveX ~= 0 and moveY ~= 0 and 0.707 or 1.00
-      entity.vx = 60 * speedMult * moveX
-      entity.vy = 55 * speedMult * moveY
+      local diagonalMult = moveX ~= 0 and moveY ~= 0 and 0.707 or 1.00
+      local speed
+      if entity.throwPhase == 'charging' then
+        speed = 20
+      elseif entity.throwPhase == 'aiming' then
+        speed = 5
+      else
+        speed = 60
+      end
+      entity.vx = 0.7 * entity.vx + 0.3 * (speed * diagonalMult * moveX)
+      entity.vy = 0.7 * entity.vy + 0.3 * (speed * 0.9 * diagonalMult * moveY)
       entity.x = entity.x + entity.vx * dt
       entity.y = entity.y + entity.vy * dt
       entity.isMoving = moveX ~= 0 or moveY ~= 0
@@ -50,46 +63,60 @@ function game.update(self, dt, isRenderable)
         entity.stillFrames = 0
       else
         entity.stillFrames = entity.stillFrames + 1
-        if entity.stillFrames > 3 then
-          entity.moveFrames = 0
-        end
+        entity.moveFrames = 0
       end
-      self:checkForBounds(entity, 0, 0, GAME_WIDTH, GAME_HEIGHT, -1.0, true)
-      -- See if there have been collisions with any bricks
+      if entity.team == 1 then
+        self:checkForBounds(entity, 2, 3, GAME_WIDTH / 2 - 2, GAME_HEIGHT - 2, -1.0, true)
+      else
+        self:checkForBounds(entity, GAME_WIDTH / 2, 3, GAME_WIDTH / 2 - 2, GAME_HEIGHT - 2, -1.0, true)
+      end
       self:forEachEntity(function(entity2)
-        if entity2.type == 'brick' then
+        -- Try to pick up balls
+        if entity2.type == 'ball' then
+          if not entity.heldBall and not entity2.isBeingHeld and entity2.vx == 0 and entity2.vy == 0 and self:entitiesOverlapping(entity, entity2) then
+            entity.heldBall = entity2.id
+            entity2.isBeingHeld = true
+            entity2.clientId = entity.clientId
+          end
+        -- See if there have been collisions with any bricks
+        elseif entity2.type == 'brick' then
           local dir, x, y, vx, vy = self:checkForEntityCollision(entity, entity2, 0.0, true)
         end
       end)
     elseif entity.type == 'ball' then
-      entity.x = entity.x + entity.vx * dt
-      entity.y = entity.y + entity.vy * dt
-      self:checkForBounds(entity, 0, 0, GAME_WIDTH, GAME_HEIGHT, -1.0, true)
-      local xNew, yNew
-      local vxOld, vyOld = entity.vx, entity.vy
-      -- See if there have been collisions with any bricks
-      self:forEachEntity(function(entity2)
-        if entity2.type == 'brick' then
-          local dir, x, y, vx, vy = self:checkForEntityCollision(entity, entity2, -1.0, false)
-          if dir then
-            xNew, yNew = x, y
-            entity.vx, entity.vy = vx, vy
-            if entity.vx ~= vxOld or entity.vy ~= vyOld then
-              entity2.scheduledForDespawn = true
+      if entity.freezeFrames > 0 then
+        entity.freezeFrames = entity.freezeFrames - 1
+      else
+        entity.x = entity.x + entity.vx * dt
+        entity.y = entity.y + entity.vy * dt
+        self:checkForBounds(entity, 0, 0, GAME_WIDTH, GAME_HEIGHT, -1.0, true)
+        local xNew, yNew
+        local vxOld, vyOld = entity.vx, entity.vy
+        -- See if there have been collisions with any bricks
+        self:forEachEntity(function(entity2)
+          if entity2.type == 'brick' then
+            local dir, x, y, vx, vy = self:checkForEntityCollision(entity, entity2, -1.0, false)
+            if dir then
+              xNew, yNew = x, y
+              entity.vx, entity.vy = vx, vy
+              if entity.vx ~= vxOld or entity.vy ~= vyOld then
+                entity2.scheduledForDespawn = true
+                entity.freezeFrames = 3
+              end
             end
           end
+        end)
+        -- Update the ball's position as a result of colliding with a brick
+        if xNew and yNew then
+          entity.x, entity.y = xNew, yNew
         end
-      end)
-      -- Update the ball's position as a result of colliding with a brick
-      if xNew and yNew then
-        entity.x, entity.y = xNew, yNew
       end
     end
   end)
   for i = #self.entities, 1, -1 do
     local entity = self.entities[i]
     if entity.scheduledForDespawn then
-      self:despawnEntity(entity)
+      -- self:despawnEntity(entity)
     end
   end
 end
@@ -99,23 +126,46 @@ function game.handleEvent(self, eventType, eventData)
   -- Spawn a new player entity for a client
   if eventType == 'spawn-player' then
     self:spawnEntity({
+      id = 'player-' .. eventData.clientId,
       type = 'player',
       clientId = eventData.clientId,
       x = eventData.x - 5,
       y = eventData.y - 4,
       vx = 0,
       vy = 0,
+      team = eventData.team,
       facingX = 1,
       facingY = -1,
       width = 10,
       height = 8,
       isMoving = false,
       moveFrames = 0,
-      stillFrames = 0
+      stillFrames = 0,
+      heldBall = nil,
+      throwPhase = nil
     })
   -- Despawn a player
   elseif eventType == 'despawn-player' then
     self:despawnEntity(self:getEntityWhere({ clientId = eventData.clientId }))
+  elseif eventType == 'charge-throw' or eventType == 'aim-throw' or eventType == 'throw' then
+    local player = self:getEntityById(eventData.playerId)
+    if player and player.heldBall then
+      if eventType == 'charge-throw' and not player.throwPhase then
+        player.throwPhase = 'charging'
+      elseif eventType == 'aim-throw' and player.throwPhase == 'charging' then
+        player.throwPhase = 'aiming'
+      elseif eventType == 'throw' and player.throwPhase == 'aiming' then
+        local ball = self:getEntityById(player.heldBall)
+        player.throwPhase = nil
+        player.heldBall = nil
+        if ball then
+          ball.isBeingHeld = false
+          ball.x = player.x + player.width / 2 - ball.width / 2
+          ball.y = player.y + player.height / 2 - ball.height / 2
+          ball.vx = 30
+        end
+      end
+    end
   end
 end
 
@@ -224,20 +274,57 @@ end
 -- Create a client-server network for the game to run on
 local network, server, client = simulsim.createGameNetwork(game, {
   exposeGameWithoutPrediction = true,
+  width = GAME_WIDTH,
+  height = GAME_HEIGHT,
+  numClients = 4,
   latency = 300
 })
 
+function server.load(self)
+  self.numTeam1Players = 0
+  self.numTeam2Players = 0
+end
+
 -- When a client connects to the server, spawn a playable entity for them to control
 function server.clientconnected(self, client)
+  -- Pick a team for the client to start on
+  local team
+  if self.numTeam1Players < self.numTeam2Players then
+    team = 1
+  elseif self.numTeam1Players > self.numTeam2Players then
+    team = 2
+  else
+    team = math.random(1, 2)
+  end
+  -- Update the player counts
+  if team == 1 then
+    self.numTeam1Players = self.numTeam1Players + 1
+  elseif team == 2 then
+    self.numTeam2Players = self.numTeam2Players + 1
+  end
+  -- Spawn a player for the client
+  local x = math.random(80, 120)
+  local y = GAME_HEIGHT / 2 + math.random(-55, 55)
   self:fireEvent('spawn-player', {
     clientId = client.clientId,
-    x = GAME_WIDTH / 2,
-    y = GAME_HEIGHT / 2
+    x = team == 1 and x or GAME_WIDTH - x,
+    y = y,
+    team = team
   })
 end
 
 -- When a client disconnects from the server, despawn their player entity
 function server.clientdisconnected(self, client)
+  -- Update the player counts
+  local player = self.game:getEntityWhere({ type = 'player', clientId = client.clientId })
+  if player then
+    if player.team == 1 then
+      self.numTeam1Players = self.numTeam1Players - 1
+    elseif player.team == 2 then
+      self.numTeam2Players = self.numTeam2Players - 1
+    end
+  end
+  -- Despawn the client's player
   self:fireEvent('despawn-player', { clientId = client.clientId })
 end
 
@@ -248,12 +335,14 @@ end
 
 -- Every frame the client tells the server which buttons it's pressing
 function client.update(self, dt)
-  self:setInputs({
-    up = love.keyboard.isDown('w') or love.keyboard.isDown('up'),
-    left = love.keyboard.isDown('a') or love.keyboard.isDown('left'),
-    down = love.keyboard.isDown('s') or love.keyboard.isDown('down'),
-    right = love.keyboard.isDown('d') or love.keyboard.isDown('right')
-  })
+  if self:isHighlighted() then
+    self:setInputs({
+      up = love.keyboard.isDown('w') or love.keyboard.isDown('up'),
+      left = love.keyboard.isDown('a') or love.keyboard.isDown('left'),
+      down = love.keyboard.isDown('s') or love.keyboard.isDown('down'),
+      right = love.keyboard.isDown('d') or love.keyboard.isDown('right')
+    })
+  end
 end
 
 -- Draw the game for each client
@@ -274,13 +363,25 @@ function client.draw(self)
     elseif entity.type == 'brick' then
       self:drawSprite(1, 17, 8, 14, entity.x - (entity.team == 1 and 2 or 0), entity.y - 1)
     elseif entity.type == 'ball' then
-      self:drawSprite(25, 22, 10, 4, entity.x - 1, entity.y + entity.height - 3)
+      if not entity.isBeingHeld then
+        self:drawSprite(25, 22, 10, 4, entity.x - 1, entity.y + entity.height - 3)
+      end
     end
+  end
+  -- Draw an indicator under your player character
+  local player = self.game:getEntityWhere({ type = 'player', clientId = self.clientId })
+  if player then
+    self:drawSprite(player.team == 1 and 53 or 36, 17, 16, 8, player.x - 3, player.y)
   end
   -- Draw each entity
   love.graphics.setColor(1, 1, 1)
   for _, entity in ipairs(self.game.entities) do
     if entity.type == 'player' then
+      local sx = 1
+      local sy = (entity.team == 2 and 33 or 118)
+      local x = entity.x
+      local y = entity.y
+      local flipHorizontal = entity.facingX < 0
       local animSprite
       if entity.isMoving then
         animSprite = 2 + math.floor((entity.moveFrames % (8 * 5)) / 5)
@@ -295,14 +396,44 @@ function client.draw(self)
       else
         dirSprite = 3
       end
-      local flipHorizontal = entity.facingX < 0
-      self:drawSprite(1 + 16 * (animSprite - 1), 33 + 17 * (dirSprite - 1), 15, 16, entity.x - (flipHorizontal and 4 or 1), entity.y - 9, flipHorizontal)
+      if entity.throwPhase == 'charging' then
+        animSprite = entity.isMoving and entity.moveFrames % 40 > 20 and 20 or 19
+        dirSprite = 3
+        x, y = x  + (player.team == 1 and -3 or 3), y - 1
+        flipHorizontal = entity.team == 2
+      elseif entity.throwPhase == 'aiming' then
+        animSprite = entity.isMoving and entity.moveFrames % 70 > 35 and 22 or 21
+        dirSprite = 3
+        x, y = x + (player.team == 1 and -3 or 3), y - 1
+        flipHorizontal = entity.team == 2
+      elseif entity.heldBall then
+        animSprite = animSprite + 9
+      end
+      self:drawSprite(sx + 16 * (animSprite - 1), sy + 17 * (dirSprite - 1), 15, 16, x - (flipHorizontal and 4 or 1), y - 9, flipHorizontal)
     elseif entity.type == 'brick' then
-      self:drawSprite(29, 1, 6, 15, entity.x, entity.y - 3, entity.team == 2)
+      local colorSprite = entity.color
+      self:drawSprite(1 + 7 * (colorSprite - 1), 1, 6, 15, entity.x, entity.y - 3, entity.team == 2)
     elseif entity.type == 'ball' then
-      self:drawSprite(68, 17, 8, 8, entity.x, entity.y - 1)
+      if not entity.isBeingHeld then
+        self:drawSprite(70, 17, 8, 8, entity.x, entity.y - 1)
+      end
     else
       love.graphics.rectangle('line', entity.x, entity.y, entity.width, entity.height)
+    end
+  end
+end
+
+function client.keypressed(self, key)
+  if key == 'space' then
+    local player = self.game:getEntityWhere({ type = 'player', clientId = self.clientId })
+    if player and player.heldBall then
+      if not player.throwPhase then
+        self:fireEvent('charge-throw', { playerId = player.id })
+      elseif player.throwPhase == 'charging' then
+        self:fireEvent('aim-throw', { playerId = player.id })
+      elseif player.throwPhase == 'aiming' then
+        self:fireEvent('throw', { playerId = player.id })
+      end
     end
   end
 end
